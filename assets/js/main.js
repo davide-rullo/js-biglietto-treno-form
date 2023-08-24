0255c9e554c49ec35f084ce5a91e0481cd26b682
davide-rullo <davide.rullo01@gmail.com>
2023-08-24 17:50:23 +0200

    Fix and add
    fixed indent issue and changed layout colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,49 @@
 const show = document.getElementById('confirm');
-
-console.log("ciao");
-
-let distance= document.getElementById("km");
+let userName = document.getElementById("nameSurname");
+let distance = document.getElementById("km");
 let userAge = document.getElementById("age");
 
- 
+
 console.log(distance);
 console.log(userAge);
 
-show.addEventListener("click", function() {
-  distance = distance.value;
-  userAge = userAge.value;
-  console.log(distance);
-  console.log(userAge);
+show.addEventListener("click", function () {
+    userName = userName.value;
+    distance = distance.value;
+    userAge = userAge.value;
+    console.log(distance);
+    console.log(userAge);
 
-  let price = distance * 0.21;
+    let price = distance * 0.21;
 
-let finalPrice;
+    let finalPrice;
 
-if (userAge < 18) {
-    price = price - (price * 20 / 100);
-    finalPrice = price.toFixed(2);
-    document.getElementById("hiddenTicket").style.display="block";
-    document.getElementById("ticketTitle").style.display="block";
-    document.getElementById("priceDOM").innerHTML = finalPrice;
+    if (userAge < 18) {
+        price = price - (price * 20 / 100);
+        finalPrice = price.toFixed(2);
+        document.getElementById("hiddenTicket").style.display = "block";
+        document.getElementById("ticketTitle").style.display = "block";
+        document.getElementById("priceDOM").innerHTML = finalPrice;
 
-} else if (userAge >= 18 && userAge < 65) {
+    } else if (userAge >= 18 && userAge < 65) {
 
-    finalPrice = price.toFixed(2);
-    document.getElementById("hiddenTicket").style.display="block";
-    document.getElementById("ticketTitle").style.display="block";
-    document.getElementById("priceDOM").innerHTML = finalPrice;
-}
-else if (userAge >= 65) {
-    price = price - (price * 40 / 100);
-    finalPrice = price.toFixed(2);
-    document.getElementById("hiddenTicket").style.display="block";
-    document.getElementById("ticketTitle").style.display="block";
-    document.getElementById("priceDOM").innerHTML = finalPrice;
-} 
+        finalPrice = price.toFixed(2);
+        document.getElementById("hiddenTicket").style.display = "block";
+        document.getElementById("ticketTitle").style.display = "block";
+        document.getElementById("priceDOM").innerHTML = finalPrice;
+    }
+    else if (userAge >= 65) {
+        price = price - (price * 40 / 100);
+        finalPrice = price.toFixed(2);
+        document.getElementById("hiddenTicket").style.display = "block";
+        document.getElementById("ticketTitle").style.display = "block";
+        document.getElementById("priceDOM").innerHTML = finalPrice;
+    }
 
-console.log(finalPrice);
+    document.getElementById("nameDOM").innerHTML = userName;
+    console.log(finalPrice);
 });
 
-console.log(distance);
-console.log(userAge);
 
 
 
